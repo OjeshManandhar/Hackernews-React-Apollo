d51b76cd3370290510a319d4901ff273df5b9301
OjeshManandhar <ozes.manandhar@gmail.com>
2020-01-17 18:46:07 +0545

    Fetch data from server

--- src/components/Link.js
+++ src/components/Link.js
@@ -4,9 +4,7 @@ class Link extends Component {
   render() {
     return (
       <div>
-        <div>
-          {this.props.link.description} ({this.props.link.url})
-        </div>
+        {this.props.link.description} ({this.props.link.url})
       </div>
     );
   }
--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,5 +1,21 @@
 import React, { Component } from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+
 import Link from './Link';
+
+const FEED_QUERY = gql`
+  {
+    feed {
+      links {
+        id
+        createdAt
+        url
+        description
+      }
+    }
+  }
+`;
 
 class LinkList extends Component {
   render() {
@@ -17,11 +33,23 @@ class LinkList extends Component {
     ];
 
     return (
-      <div>
-        {linksToRender.map(link => (
-          <Link key={link.id} link={link} />
-        ))}
-      </div>
+      <Query query={FEED_QUERY}>
+        {({ loading, error, data }) => {
+          if (loading) return <div>Fetching</div>;
+          if (error) return <div>Error</div>;
+
+          console.log('data:', data);
+          const linksToRender = data.feed.links;
+
+          return (
+            <div>
+              {linksToRender.map(link => (
+                <Link key={link.id} link={link} />
+              ))}
+            </div>
+          );
+        }}
+      </Query>
     );
   }
 }
