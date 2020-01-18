e2c8e6a8e0739133a6d7708305a8844186f80350
OjeshManandhar <ozes.manandhar@gmail.com>
2020-01-18 18:49:49 +0545

    Complete _subscribeToNewLinks

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -37,8 +37,24 @@ class LinkList extends Component {
     store.writeQuery({ query: FEED_QUERY, data });
   };
 
-  _subscribeToNewLinks = async () => {
-    // ... you'll implement this 🔜
+  _subscribeToNewLinks = subscribeToMore => {
+    subscribeToMore({
+      document: NEW_LINKS_SUBSCRIPTION,
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData.data) return prev;
+        const newLink = subscriptionData.data.newLink;
+        const exists = prev.feed.links.find(({ id }) => id === newLink.id);
+        if (exists) return prev;
+
+        return Object.assign({}, prev, {
+          feed: {
+            links: [newLink, ...prev.feed.links],
+            count: prev.feed.links.length + 1,
+            __typename: prev.feed.__typename
+          }
+        });
+      }
+    });
   };
 
   render() {
