cd541fe0150dbc090e8a862ee76ffad40594591c
OjeshManandhar <ozes.manandhar@gmail.com>
2020-01-18 11:40:07 +0545

    Navigate between pages

--- src/index.js
+++ src/index.js
@@ -4,28 +4,28 @@ import './styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-// 1
+import { BrowserRouter } from 'react-router-dom';
+
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
-// 2
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 });
 
-// 3
 const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache()
 });
 
-// 4
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
